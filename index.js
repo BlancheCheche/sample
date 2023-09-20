7e9f8a34cff3fd5cd95fa8719f630ab3682d3170
BlancheCheche <137413328+BlancheCheche@users.noreply.github.com>
2023-09-20 20:44:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log{};
+
+import { memo, useState } from "react";
+import styled from "styled-components";
+import IPPONLogo from "./ippon.png"
+
+
+const SignIn = memo()
+
+    const username = "Blanchee";
+    const password = "Blanchee";
